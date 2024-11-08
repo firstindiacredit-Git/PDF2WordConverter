6ff12b1c641178e1483b167e302a2a982c1a3088
PizeonFly <firsindiacredit786@gmail.com>
2024-11-08 11:39:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const handleFileRoute = require("./routes/handleFileRoute");
 
 //express middlewares
-app.use(cors());
+//app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
